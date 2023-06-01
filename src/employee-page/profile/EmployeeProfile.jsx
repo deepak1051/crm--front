0cec43818534adf9cf3bd562da26d1fa6f22693f
deepak1051 <lightyagamigoku@gmail.com>
2023-06-01 14:25:42 +0530

    hello world

--- src/employee-page/profile/EmployeeProfile.jsx
+++ src/employee-page/profile/EmployeeProfile.jsx
@@ -1,10 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { AiOutlineEdit, AiOutlineUser } from 'react-icons/ai';
 import Chart from '../../extra/chart/Chart';
 import { fetchSingleEmployee } from '../../store';
 
+function convertToBase64(file) {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
+
+const default_img =
+  'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg?w=740&t=st=1685609599~exp=1685610199~hmac=c8d8f239a28668434bbcd8177d2141a2495fa383861c41077b42e5f381ebedf8';
 const EmployeeProfile = () => {
+  const [postImage, setPostImage] = useState(() =>
+    localStorage.getItem('logo')
+  );
   const { singleEmployee } = useSelector((state) => state.admin);
   const { id } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -12,6 +30,16 @@ const EmployeeProfile = () => {
   useEffect(() => {
     dispatch(fetchSingleEmployee({ id }));
   }, [dispatch, id]);
+
+  const handlePhoto = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    // const base64 = await convertToBase64(file);
+
+    setPostImage(base64);
+
+    localStorage.setItem('logo', base64);
+  };
 
   return (
     <div
@@ -21,12 +49,50 @@ const EmployeeProfile = () => {
       <div className="top">
         <div className="left">
           <h1 className="title">Information</h1>
-          <div className="item">
+
+          <div style={{ position: 'relative' }}>
             <img
+              src={postImage ? postImage : default_img}
+              alt=""
+              style={{ width: '125px', height: '125px' }}
+            />
+            <label
+              htmlFor="pic"
+              style={{
+                cursor: 'pointer',
+                position: 'absolute',
+                top: '70%',
+                right: '20%',
+                border: '1px solid black',
+                background: 'white',
+                padding: '5px',
+                borderRadius: '5px',
+                fontSize: '14px',
+                display: 'flex',
+                alignItems: 'center',
+                gap: '4px',
+              }}
+            >
+              Edit
+              <AiOutlineEdit />
+            </label>
+            <input
+              type="file"
+              label="Image"
+              accept=".png, .jpg, .jpeg"
+              name="profilePic"
+              id="pic"
+              onChange={handlePhoto}
+              style={{ display: 'none' }}
+            />
+          </div>
+
+          <div className="item">
+            {/* <img
               src={`https://picsum.photos/seed/${id}/300/200`}
               alt=""
               className="itemImg"
-            />
+            /> */}
             <div className="single-list__container">
               <h1
                 className="itemTitle"
